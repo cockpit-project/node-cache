70243505b6c3bb9e1c0c1b700608367afe4a898e
GitHub Workflow <cockpituous@cockpit-project.org>
2021-08-31 11:17:48 +0000

    PR: https://github.com/cockpit-project/cockpit/pull/16279
    npm WARN deprecated uuid@3.4.0: Please upgrade  to version 7 or higher.  Older versions may use Math.random() in certain circumstances, which is known to be problematic.  See https://v8.dev/blog/math-random for details.
    npm WARN deprecated core-js@1.2.7: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm WARN deprecated popper.js@1.16.1: You can find the new Popper v2 at @popperjs/core, this package is dedicated to the legacy v1
    npm WARN deprecated datatables.net-bs@2.1.1: Incorrect version published. Please use the latest non-deprecated version - 1.10.13 at the time of writing
    npm WARN deprecated core-js@2.6.12: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm WARN deprecated libcipm@4.0.8: This module is no longer used. Please see @npmcli/arborist if you would like to build and reify package trees programmatically.
    npm WARN deprecated request@2.88.2: request has been deprecated, see https://github.com/request/request/issues/3142
    npm WARN deprecated har-validator@5.1.5: this library is no longer supported
    npm notice created a lockfile as package-lock.json. You should commit this file.
    npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.2 (node_modules/chokidar/node_modules/fsevents):
    npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
    npm WARN react-ellipsis-with-tooltip@1.1.1 requires a peer of react-bootstrap@0.31.x || 0.32.x but none is installed. You must install peer dependencies yourself.
    npm WARN table-resolver@3.3.0 requires a peer of redux@>= 3.0.0 < 4.0.0 but none is installed. You must install peer dependencies yourself.
    npm WARN @octokit/plugin-request-log@1.0.4 requires a peer of @octokit/core@>=3 but none is installed. You must install peer dependencies yourself.
    npm WARN acorn-import-assertions@1.7.6 requires a peer of acorn@^8 but none is installed. You must install peer dependencies yourself.
    
    added 1606 packages from 1585 contributors and audited 1610 packages in 68.849s
    
    126 packages are looking for funding
      run `npm fund` for details
    
    found 10 vulnerabilities (1 low, 6 moderate, 3 high)
      run `npm audit fix` to fix them, or `npm audit` for details

---
+++ @patternfly/react-core/dist/esm/layouts/Flex/FlexItem.js
@@ -0,0 +1,12 @@
+import { __rest } from "tslib";
+import * as React from 'react';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/react-styles/css/layouts/Flex/flex';
+import * as flexToken from "@patternfly/react-tokens/dist/esm/l_flex_item_Order";
+import { formatBreakpointMods, setBreakpointCssVars } from '../../helpers/util';
+export const FlexItem = (_a) => {
+    var { children = null, className = '', spacer, grow, shrink, flex, alignSelf, align, fullWidth, order, style } = _a, props = __rest(_a, ["children", "className", "spacer", "grow", "shrink", "flex", "alignSelf", "align", "fullWidth", "order", "style"]);
+    return (React.createElement("div", Object.assign({}, props, { className: css(formatBreakpointMods(spacer, styles), formatBreakpointMods(grow, styles), formatBreakpointMods(shrink, styles), formatBreakpointMods(flex, styles), formatBreakpointMods(alignSelf, styles), formatBreakpointMods(align, styles), formatBreakpointMods(fullWidth, styles), className), style: style || order ? Object.assign(Object.assign({}, style), setBreakpointCssVars(order, flexToken.l_flex_item_Order.name)) : undefined }), children));
+};
+FlexItem.displayName = 'FlexItem';
+//# sourceMappingURL=FlexItem.js.map
