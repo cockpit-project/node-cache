f233df5d657a3651f51d30db59d5f315594d78a1
GitHub Workflow <cockpituous@cockpit-project.org>
2021-09-13 03:09:13 +0000

    PR: https://github.com/cockpit-project/cockpit/pull/16340
    npm WARN deprecated uuid@3.4.0: Please upgrade  to version 7 or higher.  Older versions may use Math.random() in certain circumstances, which is known to be problematic.  See https://v8.dev/blog/math-random for details.
    npm WARN deprecated core-js@1.2.7: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm WARN deprecated popper.js@1.16.1: You can find the new Popper v2 at @popperjs/core, this package is dedicated to the legacy v1
    npm WARN deprecated core-js@2.6.12: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm WARN deprecated libcipm@4.0.8: This module is no longer used. Please see @npmcli/arborist if you would like to build and reify package trees programmatically.
    npm WARN deprecated request@2.88.2: request has been deprecated, see https://github.com/request/request/issues/3142
    npm WARN deprecated har-validator@5.1.5: this library is no longer supported
    npm notice created a lockfile as package-lock.json. You should commit this file.
    npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.2 (node_modules/chokidar/node_modules/fsevents):
    npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
    npm WARN react-ellipsis-with-tooltip@1.1.1 requires a peer of react-bootstrap@0.31.x || 0.32.x but none is installed. You must install peer dependencies yourself.
    npm WARN table-resolver@3.3.0 requires a peer of redux@>= 3.0.0 < 4.0.0 but none is installed. You must install peer dependencies yourself.
    npm WARN @octokit/plugin-request-log@1.0.4 requires a peer of @octokit/core@>=3 but none is installed. You must install peer dependencies yourself.
    npm WARN acorn-import-assertions@1.7.6 requires a peer of acorn@^8 but none is installed. You must install peer dependencies yourself.
    
    added 1601 packages from 1586 contributors and audited 1604 packages in 97.82s
    
    127 packages are looking for funding
      run `npm fund` for details
    
    found 10 vulnerabilities (1 low, 6 moderate, 3 high)
      run `npm audit fix` to fix them, or `npm audit` for details

---
+++ @patternfly/react-styles/css/components/Banner/banner.d.ts
@@ -0,0 +1,14 @@
+import './banner.css';
+declare const _default: {
+  "banner": "pf-c-banner",
+  "button": "pf-c-button",
+  "card": "pf-c-card",
+  "modifiers": {
+    "warning": "pf-m-warning",
+    "info": "pf-m-info",
+    "danger": "pf-m-danger",
+    "success": "pf-m-success",
+    "sticky": "pf-m-sticky"
+  }
+};
+export default _default;
