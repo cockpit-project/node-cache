ce4bec068dcbbedaa6a600380880139fece0d8ec
GitHub Workflow <cockpituous@cockpit-project.org>
2021-10-29 08:15:09 +0000

    PR: https://github.com/cockpit-project/cockpit/pull/16513
    npm WARN deprecated popper.js@1.16.1: You can find the new Popper v2 at @popperjs/core, this package is dedicated to the legacy v1
    npm WARN deprecated core-js@2.6.12: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm notice created a lockfile as package-lock.json. You should commit this file.
    npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.2 (node_modules/chokidar/node_modules/fsevents):
    npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
    npm WARN acorn-import-assertions@1.8.0 requires a peer of acorn@^8 but none is installed. You must install peer dependencies yourself.
    
    added 771 packages from 441 contributors and audited 775 packages in 79.036s
    
    117 packages are looking for funding
      run `npm fund` for details
    
    found 4 vulnerabilities (3 moderate, 1 high)
      run `npm audit fix` to fix them, or `npm audit` for details

---
+++ @patternfly/react-styles/css/components/Table/table-scrollable.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+require('./table-scrollable.css');
+exports.default = {
+  "modifiers": {
+    "borderRight": "pf-m-border-right",
+    "stickyHeader": "pf-m-sticky-header"
+  },
+  "scrollInnerWrapper": "pf-c-scroll-inner-wrapper",
+  "scrollOuterWrapper": "pf-c-scroll-outer-wrapper",
+  "table": "pf-c-table",
+  "tableStickyColumn": "pf-c-table__sticky-column"
+};
---
+++ @patternfly/react-tokens/dist/esm/c_context_selector__toggle_BorderRightColor.js
@@ -0,0 +1,6 @@
+export const c_context_selector__toggle_BorderRightColor = {
+  "name": "--pf-c-context-selector__toggle--BorderRightColor",
+  "value": "#3c3f42",
+  "var": "var(--pf-c-context-selector__toggle--BorderRightColor)"
+};
+export default c_context_selector__toggle_BorderRightColor;
