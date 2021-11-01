d8130b88ad64b60645c10d8ec18001e3d6540edd
GitHub Workflow <cockpituous@cockpit-project.org>
2021-11-01 08:00:25 +0000

    PR: https://github.com/cockpit-project/cockpit/pull/16543
    npm WARN deprecated popper.js@1.16.1: You can find the new Popper v2 at @popperjs/core, this package is dedicated to the legacy v1
    npm WARN deprecated core-js@2.6.12: core-js@<3.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Please, upgrade your dependencies to the actual version of core-js.
    npm notice created a lockfile as package-lock.json. You should commit this file.
    npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.2 (node_modules/chokidar/node_modules/fsevents):
    npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
    npm WARN acorn-import-assertions@1.8.0 requires a peer of acorn@^8 but none is installed. You must install peer dependencies yourself.
    
    added 772 packages from 441 contributors and audited 776 packages in 57.935s
    
    117 packages are looking for funding
      run `npm fund` for details
    
    found 4 vulnerabilities (3 moderate, 1 high)
      run `npm audit fix` to fix them, or `npm audit` for details

---
+++ @patternfly/react-core/src/components/Sidebar/SidebarContent.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/react-styles/css/components/Sidebar/sidebar';
+
+export interface SidebarContentProps extends React.HTMLProps<HTMLDivElement> {
+  children: React.ReactNode;
+  hasNoBackground?: boolean;
+}
+
+export const SidebarContent: React.FunctionComponent<SidebarContentProps> = ({
+  className,
+  children,
+  hasNoBackground,
+  ...props
+}: SidebarContentProps) => (
+  <div className={css(styles.sidebarContent, hasNoBackground && styles.modifiers.noBackground, className)} {...props}>
+    {children}
+  </div>
+);
+SidebarContent.displayName = 'SidebarContent';
