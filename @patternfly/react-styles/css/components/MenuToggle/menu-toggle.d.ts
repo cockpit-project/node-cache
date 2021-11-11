8e7c657db13a4e5700b651d8dd914775cf3ccc45
GitHub Workflow <cockpituous@cockpit-project.org>
2021-11-11 09:44:34 +0000

    PR: https://github.com/cockpit-project/cockpit/pull/16593
    npm WARN deprecated popper.js@1.16.1: You can find the new Popper v2 at @popperjs/core, this package is dedicated to the legacy v1
    npm notice created a lockfile as package-lock.json. You should commit this file.
    npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.2 (node_modules/chokidar/node_modules/fsevents):
    npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
    npm WARN acorn-import-assertions@1.8.0 requires a peer of acorn@^8 but none is installed. You must install peer dependencies yourself.
    
    added 765 packages from 439 contributors and audited 769 packages in 56.62s
    
    117 packages are looking for funding
      run `npm fund` for details
    
    found 10 vulnerabilities (9 moderate, 1 high)
      run `npm audit fix` to fix them, or `npm audit` for details

---
+++ @patternfly/react-styles/css/components/MenuToggle/menu-toggle.d.ts
@@ -0,0 +1,17 @@
+import './menu-toggle.css';
+declare const _default: {
+  "menuToggle": "pf-c-menu-toggle",
+  "menuToggleControls": "pf-c-menu-toggle__controls",
+  "menuToggleCount": "pf-c-menu-toggle__count",
+  "menuToggleIcon": "pf-c-menu-toggle__icon",
+  "menuToggleText": "pf-c-menu-toggle__text",
+  "menuToggleToggleIcon": "pf-c-menu-toggle__toggle-icon",
+  "modifiers": {
+    "primary": "pf-m-primary",
+    "plain": "pf-m-plain",
+    "text": "pf-m-text",
+    "expanded": "pf-m-expanded",
+    "fullHeight": "pf-m-full-height"
+  }
+};
+export default _default;
---
+++ @patternfly/react-styles/css/components/MenuToggle/menu-toggle.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+require('./menu-toggle.css');
+exports.default = {
+  "menuToggle": "pf-c-menu-toggle",
+  "menuToggleControls": "pf-c-menu-toggle__controls",
+  "menuToggleCount": "pf-c-menu-toggle__count",
+  "menuToggleIcon": "pf-c-menu-toggle__icon",
+  "menuToggleText": "pf-c-menu-toggle__text",
+  "menuToggleToggleIcon": "pf-c-menu-toggle__toggle-icon",
+  "modifiers": {
+    "primary": "pf-m-primary",
+    "plain": "pf-m-plain",
+    "text": "pf-m-text",
+    "expanded": "pf-m-expanded",
+    "fullHeight": "pf-m-full-height"
+  }
+};
