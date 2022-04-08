0e3ad9b6aeaa874c7f4f8e5cec6e701f2453ac1f
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-04-08 11:36:10 +0200

    ca4c3d9d4450c437ccb7909d7db10f101e9239017853099429a8ea35e187328d  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_m_icon_sprite_m_invalid_BackgroundUrl.js
@@ -0,0 +1,6 @@
+export const c_form_control_m_icon_sprite_m_invalid_BackgroundUrl = {
+  "name": "--pf-c-form-control--m-icon-sprite--m-invalid--BackgroundUrl",
+  "value": "url(\"../../assets/images/status-icon-sprite.svg#invalid\")",
+  "var": "var(--pf-c-form-control--m-icon-sprite--m-invalid--BackgroundUrl)"
+};
+export default c_form_control_m_icon_sprite_m_invalid_BackgroundUrl;
