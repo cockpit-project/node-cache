59434c66485c5cfdd2dbdbe7e9cf96ffe8f0584c
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-04-12 14:20:04 +0200

    7abc91987ff4ccaf6d7a97117de088de5d14e1407475883dd6e95374e29df608  package.json

---
+++ @patternfly/react-core/src/components/Dropdown/__tests__/DropdownToggleAction.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DropdownToggleAction } from '../DropdownToggleAction';
+
+test('renders with text', () => {
+  const view = render(<DropdownToggleAction id="action" aria-label="action" />);
+  expect(view.container).toMatchSnapshot();
+});
+
+test('isDisabled', () => {
+  const view = render(<DropdownToggleAction id="action" aria-label="action" isDisabled />);
+  expect(view.container).toMatchSnapshot();
+});
+
+test('passing class', () => {
+  const view = render(<DropdownToggleAction id="action" aria-label="action" className="abc" />);
+  expect(view.container).toMatchSnapshot();
+});
+
+test('checkbox passes value and event to onClick handler', () => {
+  const onClickMock = jest.fn();
+
+  render(<DropdownToggleAction id="action" aria-label="acton" onClick={onClickMock} />);
+
+  userEvent.click(screen.getByRole('button'));
+  expect(onClickMock).toBeCalled();
+});
---
+++ @patternfly/react-core/src/layouts/Gallery/__tests__/Gallery.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Gallery } from '../Gallery';
+import { GalleryItem } from '../GalleryItem';
+import { render } from '@testing-library/react';
+
+test('gutter', () => {
+  const view = render(<Gallery hasGutter />);
+  expect(view.container).toMatchSnapshot();
+});
+
+test('gutter breakpoints', () => {
+  const view = render(
+    <Gallery
+      hasGutter
+      minWidths={{
+        default: '100%',
+        md: '100px',
+        xl: '300px'
+      }}
+      maxWidths={{
+        md: '200px',
+        xl: '1fr'
+      }}
+    />
+  );
+  expect(view.container).toMatchSnapshot();
+});
+
+test('alternative component', () => {
+  const view = render(
+    <Gallery component='ul'>
+      <GalleryItem component='li'>Test</GalleryItem>
+    </Gallery>
+  );
+  expect(view.container).toMatchSnapshot();
+});
