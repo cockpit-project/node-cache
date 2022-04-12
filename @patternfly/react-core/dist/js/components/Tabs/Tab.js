5e4588a266088feb75056153dd32ca52d08c79fd
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-04-12 18:50:30 +0200

    127e3fe4c127619cde222f899da529d9d51b76db258a462f29364873f7eca674  package.json

---
+++ @patternfly/react-core/dist/js/components/Tabs/Tab.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Tab = void 0;
+const tslib_1 = require("tslib");
+const React = tslib_1.__importStar(require("react"));
+const tabs_1 = tslib_1.__importDefault(require("@patternfly/react-styles/css/components/Tabs/tabs"));
+const TabButton_1 = require("./TabButton");
+const TabsContext_1 = require("./TabsContext");
+const react_styles_1 = require("@patternfly/react-styles");
+const Tooltip_1 = require("../Tooltip");
+const TabBase = (_a) => {
+    var { title, eventKey, tabContentRef, id: childId, tabContentId, className: childClassName = '', ouiaId: childOuiaId, isDisabled, isAriaDisabled, inoperableEvents = ['onClick', 'onKeyPress'], href, innerRef, tooltip } = _a, props = tslib_1.__rest(_a, ["title", "eventKey", "tabContentRef", "id", "tabContentId", "className", "ouiaId", "isDisabled", "isAriaDisabled", "inoperableEvents", "href", "innerRef", "tooltip"]);
+    const preventedEvents = inoperableEvents.reduce((handlers, eventToPrevent) => (Object.assign(Object.assign({}, handlers), { [eventToPrevent]: (event) => {
+            event.preventDefault();
+        } })), {});
+    const { mountOnEnter, localActiveKey, unmountOnExit, uniqueId, handleTabClick } = React.useContext(TabsContext_1.TabsContext);
+    let ariaControls = tabContentId ? `${tabContentId}` : `pf-tab-section-${eventKey}-${childId || uniqueId}`;
+    if ((mountOnEnter || unmountOnExit) && eventKey !== localActiveKey) {
+        ariaControls = undefined;
+    }
+    const isButtonElement = Boolean(!href);
+    const getDefaultTabIdx = () => {
+        if (isDisabled) {
+            return isButtonElement ? null : -1;
+        }
+        else if (isAriaDisabled) {
+            return null;
+        }
+    };
+    const tabButton = (React.createElement(TabButton_1.TabButton, Object.assign({ parentInnerRef: innerRef, className: react_styles_1.css(tabs_1.default.tabsLink, isDisabled && href && tabs_1.default.modifiers.disabled, isAriaDisabled && tabs_1.default.modifiers.ariaDisabled), disabled: isButtonElement ? isDisabled : null, "aria-disabled": isDisabled || isAriaDisabled, tabIndex: getDefaultTabIdx(), onClick: (event) => handleTabClick(event, eventKey, tabContentRef) }, (isAriaDisabled ? preventedEvents : null), { id: `pf-tab-${eventKey}-${childId || uniqueId}`, "aria-controls": ariaControls, tabContentRef: tabContentRef, ouiaId: childOuiaId, href: href }, props), title));
+    return (React.createElement("li", { className: react_styles_1.css(tabs_1.default.tabsItem, eventKey === localActiveKey && tabs_1.default.modifiers.current, childClassName) }, tooltip ? React.createElement(Tooltip_1.Tooltip, Object.assign({}, tooltip.props), tabButton) : tabButton));
+};
+exports.Tab = React.forwardRef((props, ref) => React.createElement(TabBase, Object.assign({ innerRef: ref }, props)));
+exports.Tab.displayName = 'Tab';
+//# sourceMappingURL=Tab.js.map
---
+++ @patternfly/react-core/src/components/OptionsMenu/__tests__/Generated/OptionsMenuItemGroup.test.tsx
@@ -0,0 +1,21 @@
+/**
+ * This test was generated
+ */
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { OptionsMenuItemGroup } from '../../OptionsMenuItemGroup';
+// any missing imports can usually be resolved by adding them here
+import {} from '../..';
+
+it('OptionsMenuItemGroup should match snapshot (auto-generated)', () => {
+  const view = render(
+    <OptionsMenuItemGroup
+      children={<>ReactNode</>}
+      className={"''"}
+      ariaLabel={"''"}
+      groupTitle={''}
+      hasSeparator={false}
+    />
+  );
+  expect(view.container).toMatchSnapshot();
+});
