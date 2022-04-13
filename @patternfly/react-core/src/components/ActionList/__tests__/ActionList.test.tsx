c72ab6e8e2ca20666d31653ca3b5f7e4b140d579
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2022-04-13 10:45:27 +0200

    e46627adb28213ceaf0acf740bce7ad5eab664e2f55d783b1367815634b417b3  package.json

---
+++ @patternfly/react-core/src/components/ActionList/__tests__/ActionList.test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { ActionList } from '../ActionList';
+
+describe('action list', () => {
+  test('renders successfully', () => {
+    const view = render(<ActionList>test</ActionList>);
+    expect(view.container).toMatchSnapshot();
+  });
+  test('isIconList flag adds modifier', () => {
+    const view = render(<ActionList isIconList>test</ActionList>);
+    expect(view.container).toMatchSnapshot();
+  });
+});
