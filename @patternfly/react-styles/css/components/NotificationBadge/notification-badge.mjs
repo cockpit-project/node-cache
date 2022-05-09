a5a8ddcf57c9a489b8e593c87805522c7ad6d5b8
GitHub Workflow <cockpituous@cockpit-project.org>
2022-05-09 03:32:00 +0000

    89ab29f2ec783bdfbfa39d293e33bb5a06cb148f94cf70cc4235f1effae9096f  package.json

---
+++ @patternfly/react-styles/css/components/NotificationBadge/notification-badge.mjs
@@ -0,0 +1,12 @@
+import('./notification-badge.css');
+export default {
+  "iconAttentionBell": "pf-icon-attention-bell",
+  "iconBell": "pf-icon-bell",
+  "modifiers": {
+    "read": "pf-m-read",
+    "unread": "pf-m-unread",
+    "attention": "pf-m-attention"
+  },
+  "notificationBadge": "pf-c-notification-badge",
+  "notificationBadgeCount": "pf-c-notification-badge__count"
+};
