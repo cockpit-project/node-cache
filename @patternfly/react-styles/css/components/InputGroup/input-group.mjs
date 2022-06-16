da0f12c95240aac06643d478ddd180e64810b220
Jelle van der Waa <jelle@vdwaa.nl>
2022-06-16 15:32:38 +0200

    d7e2564cdc32aad43ddacd673de08a30ad4dd816438b0217775fe63c3a9ae26f  package.json

---
+++ @patternfly/react-styles/css/components/InputGroup/input-group.mjs
@@ -0,0 +1,9 @@
+import('./input-group.css');
+export default {
+  "formControl": "pf-c-form-control",
+  "inputGroup": "pf-c-input-group",
+  "inputGroupText": "pf-c-input-group__text",
+  "modifiers": {
+    "plain": "pf-m-plain"
+  }
+};
