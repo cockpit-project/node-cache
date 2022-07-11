39e50244d5cee9c1e42c838ccc32ad89f5d81c4f
GitHub Workflow <cockpituous@cockpit-project.org>
2022-07-11 03:39:46 +0000

    6a2333aad542744d66afbc3300952687e0a83946d7a4bde33852977c400901a2  package.json

---
+++ @patternfly/react-core/dist/esm/components/DualListSelector/DualListSelectorList.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface DualListSelectorListProps extends React.HTMLProps<HTMLUListElement> {
+    /** Content rendered inside the dual list selector list */
+    children?: React.ReactNode;
+}
+export declare const DualListSelectorList: React.FunctionComponent<DualListSelectorListProps>;
+//# sourceMappingURL=DualListSelectorList.d.ts.map
