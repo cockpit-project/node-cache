b0921b44f74ae2c1f74d9e4f9494863349a248ed
Jelle van der Waa <jelle@vdwaa.nl>
2022-07-20 16:12:06 +0200

    3f1e308f7371149070d93b7b9056501eda341c646056934ba5684af64db08603  package.json

---
+++ @patternfly/react-console/dist/js/components/DesktopViewer/MoreInformationInstallVariant.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+export interface MoreInformationInstallVariantProps {
+    children?: React.ReactNode;
+    os: string;
+    content?: string | React.ReactNode;
+}
+export declare const MoreInformationInstallVariant: React.FunctionComponent<MoreInformationInstallVariantProps>;
+//# sourceMappingURL=MoreInformationInstallVariant.d.ts.map
