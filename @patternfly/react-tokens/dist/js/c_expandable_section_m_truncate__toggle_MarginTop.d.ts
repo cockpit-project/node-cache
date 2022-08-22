76da54f0f2e3257e832686fd7c5c06b9cc385a03
GitHub Workflow <cockpituous@cockpit-project.org>
2022-08-22 06:45:01 +0000

    c9cc6be64e4bfe0ab6a20c0ef0a694f5b2aa9813961361289b58265a37f801e5  package.json

---
+++ @patternfly/react-tokens/dist/js/c_expandable_section_m_truncate__toggle_MarginTop.d.ts
@@ -0,0 +1,6 @@
+export const c_expandable_section_m_truncate__toggle_MarginTop: {
+  "name": "--pf-c-expandable-section--m-truncate__toggle--MarginTop",
+  "value": "0.25rem",
+  "var": "var(--pf-c-expandable-section--m-truncate__toggle--MarginTop)"
+};
+export default c_expandable_section_m_truncate__toggle_MarginTop;
