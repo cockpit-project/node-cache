429a1b7892a39812930f8a715459c6260bf1d577
GitHub Workflow <cockpituous@cockpit-project.org>
2022-11-21 03:17:50 +0000

    ae589d8da305e11982f32afb9b3037f1176448b9ee1be76250fe991c7a6681b1  package.json

---
+++ @patternfly/react-core/dist/js/components/HelperText/__mocks__/HelperText.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { HelperTextProps } from '../';
+export declare const HelperText: ({ children, isLiveRegion }: HelperTextProps) => JSX.Element;
+//# sourceMappingURL=HelperText.d.ts.map
