71e7498c037fe121f6bbff308c2007b6e3adf42d
Katerina Koukiou <kkoukiou@redhat.com>
2022-11-23 11:40:46 +0100

    d102879df38263094533a58a438d5c356ee22b2a227ae99faf0250dc69fbec02  package.json

---
+++ @patternfly/react-core/dist/esm/components/Button/__mocks__/Button.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import { ButtonProps } from '../';
+export declare const Button: ({ children, variant, isInline, onClick, iconPosition, icon, ...props }: ButtonProps) => JSX.Element;
+export declare const ButtonVariant: {
+    plain: string;
+    link: string;
+};
+//# sourceMappingURL=Button.d.ts.map
