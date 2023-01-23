08bb5b88535efd29fa8ed45b4567ac69196079e1
GitHub Workflow <cockpituous@cockpit-project.org>
2023-01-23 02:42:43 +0000

    ca89de4c4dd0729b66ebb3f11d8895c7360306dde601b739924fb57d1bbb41cf  package.json

---
+++ @patternfly/react-core/dist/js/next/components/Wizard/WizardStep.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WizardStep = void 0;
+const tslib_1 = require("tslib");
+const react_1 = tslib_1.__importDefault(require("react"));
+const types_1 = require("./types");
+const WizardContext_1 = require("./WizardContext");
+const WizardStep = (_a) => {
+    var { children, steps: _subSteps } = _a, props = tslib_1.__rest(_a, ["children", "steps"]);
+    const { activeStep, setStep } = (0, WizardContext_1.useWizardContext)();
+    const { id, name, body, isDisabled, isHidden, navItem, footer, status } = props;
+    // Update step in context when props change or when the step is active has yet to be marked as visited.
+    react_1.default.useEffect(() => {
+        const shouldUpdateIsVisited = !(0, types_1.isWizardParentStep)(activeStep) && id === (activeStep === null || activeStep === void 0 ? void 0 : activeStep.id) && !(activeStep === null || activeStep === void 0 ? void 0 : activeStep.isVisited);
+        setStep(Object.assign(Object.assign(Object.assign(Object.assign(Object.assign(Object.assign(Object.assign({ id,
+            name }, (body && { body })), (isDisabled && { isDisabled })), (isHidden && { isHidden })), (navItem && { navItem })), (footer && { footer })), (status && { status })), (shouldUpdateIsVisited && { isVisited: true })));
+    }, [body, footer, id, isDisabled, isHidden, name, navItem, status, activeStep, setStep]);
+    return react_1.default.createElement(react_1.default.Fragment, null, children);
+};
+exports.WizardStep = WizardStep;
+exports.WizardStep.displayName = 'WizardStep';
+//# sourceMappingURL=WizardStep.js.map
