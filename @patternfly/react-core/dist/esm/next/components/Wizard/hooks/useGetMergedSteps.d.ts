1086ff2e40d6490c81eb49dc9b6881446d59f70f
GitHub Workflow <cockpituous@cockpit-project.org>
2023-01-23 03:06:52 +0000

    c517e8faa3ca825fcb7b75344497b6d35403a5bbdd56511c1c20eec0c9df3101  package.json

---
+++ @patternfly/react-core/dist/esm/next/components/Wizard/hooks/useGetMergedSteps.d.ts
@@ -0,0 +1,9 @@
+import { WizardStepType } from '../types';
+/**
+ * Get stable ref of combined initial (prop provided) and current (context stored) steps.
+ * @param initialSteps
+ * @param currentSteps
+ * @returns WizardStepType[]
+ */
+export declare const useGetMergedSteps: (initialSteps: WizardStepType[], currentSteps: WizardStepType[]) => WizardStepType[];
+//# sourceMappingURL=useGetMergedSteps.d.ts.map
