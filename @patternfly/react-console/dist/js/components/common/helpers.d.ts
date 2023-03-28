f68eb01b9e11a8c097a264c161350ce51c9f6994
Katerina Koukiou <kkoukiou@redhat.com>
2023-03-28 09:36:05 +0200

    8ec33011d6214e5ff1fb9d73c03e2784f21654b98ab6adbdfc2906112934beac  package.json

---
+++ @patternfly/react-console/dist/js/components/common/helpers.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+export declare const child: (children: React.ReactNode, index: number) => string | number | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment;
+//# sourceMappingURL=helpers.d.ts.map
