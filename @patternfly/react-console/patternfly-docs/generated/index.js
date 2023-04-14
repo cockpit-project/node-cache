f749c6d7890dda88e6e7e86dd99f31c84f755c7c
Martin Pitt <martin@piware.de>
2023-04-14 06:56:12 +0200

    cd6a34d6bdcbcf211b3bcf34177738592acb1d98ed9cbb5b89abaa5913ea5292  package.json

---
+++ @patternfly/react-console/patternfly-docs/generated/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  '/extensions/react-console/react': {
+    id: "React console",
+    title: "React console",
+    toc: [[{"text":"Note"}],{"text":"Examples"},[{"text":"Basic Usage"}]],
+    fullscreenExamples: ["Basic Usage"],
+    section: "extensions",
+    subsection: "",
+    source: "react",
+    tabName: null,
+    Component: () => import(/* webpackChunkName: "extensions/react-console/react/index" */ './extensions/react-console/react')
+  }
+};
