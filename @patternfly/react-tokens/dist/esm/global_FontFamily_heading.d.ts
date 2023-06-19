50672b21f4468cc7c249e1ec81670ceede020fc7
GitHub Workflow <cockpituous@cockpit-project.org>
2023-06-19 03:07:04 +0000

    ec8fc40d356dfa629b37e4071395fd937282e19e5bb17ac94dcdf0108752ea67  package.json

---
+++ @patternfly/react-tokens/dist/esm/global_FontFamily_heading.d.ts
@@ -0,0 +1,6 @@
+export const global_FontFamily_heading: {
+  "name": "--pf-v5-global--FontFamily--heading",
+  "value": "'\"RedHatDisplayVF\", \"RedHatDisplay\", helvetica, arial, sans-serif'",
+  "var": "var(--pf-v5-global--FontFamily--heading)"
+};
+export default global_FontFamily_heading;
