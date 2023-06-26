e315af6b7f056fd62cd15900507031dbb5a1f5ab
GitHub Workflow <cockpituous@cockpit-project.org>
2023-06-26 03:19:54 +0000

    ec8fc40d356dfa629b37e4071395fd937282e19e5bb17ac94dcdf0108752ea67  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_m_error_BorderBottomColor.d.ts
@@ -0,0 +1,6 @@
+export const c_form_control_m_error_BorderBottomColor: {
+  "name": "--pf-v5-c-form-control--m-error--BorderBottomColor",
+  "value": "#c9190b",
+  "var": "var(--pf-v5-c-form-control--m-error--BorderBottomColor)"
+};
+export default c_form_control_m_error_BorderBottomColor;
