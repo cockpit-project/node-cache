be9f25d1bbaed8f33460247e86c898688828ead5
Martin Pitt <martin@piware.de>
2023-07-21 09:11:52 +0200

    ec8fc40d356dfa629b37e4071395fd937282e19e5bb17ac94dcdf0108752ea67  package.json

---
+++ @patternfly/react-tokens/dist/js/c_form_control_m_error_BorderBottomWidth.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_form_control_m_error_BorderBottomWidth = {
+  "name": "--pf-v5-c-form-control--m-error--BorderBottomWidth",
+  "value": "2px",
+  "var": "var(--pf-v5-c-form-control--m-error--BorderBottomWidth)"
+};
+exports["default"] = exports.c_form_control_m_error_BorderBottomWidth;
