27c37a976b6fce147d08dae5ca015a9e56a10180
Martin Pitt <martin@piware.de>
2023-07-25 19:33:20 +0200

    49112124244e45b6bcd79595ae5ba5367c30cafaafcee6f3b3c5f41e7abaed42  package.json

---
+++ @patternfly/react-tokens/dist/js/c_form_control_m_readonly_focus_BorderBottomWidth.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_form_control_m_readonly_focus_BorderBottomWidth = {
+  "name": "--pf-v5-c-form-control--m-readonly--focus--BorderBottomWidth",
+  "value": "1px",
+  "var": "var(--pf-v5-c-form-control--m-readonly--focus--BorderBottomWidth)"
+};
+exports["default"] = exports.c_form_control_m_readonly_focus_BorderBottomWidth;
