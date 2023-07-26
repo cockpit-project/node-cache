42cf14455670277931a945beb68b031057c005a6
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-26 22:23:00 +0200

    1a6f3f9e5d997793c9407863c0c9d609631dcc90a3dcc5c2bfa38aa417e55183  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_m_success_BorderBottomColor.d.ts
@@ -0,0 +1,6 @@
+export const c_form_control_m_success_BorderBottomColor: {
+  "name": "--pf-v5-c-form-control--m-success--BorderBottomColor",
+  "value": "#3e8635",
+  "var": "var(--pf-v5-c-form-control--m-success--BorderBottomColor)"
+};
+export default c_form_control_m_success_BorderBottomColor;
---
+++ @patternfly/react-tokens/dist/js/c_form_control_BorderWidth.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_form_control_BorderWidth = {
+  "name": "--pf-v5-c-form-control--BorderWidth",
+  "value": "1px",
+  "var": "var(--pf-v5-c-form-control--BorderWidth)"
+};
+exports["default"] = exports.c_form_control_BorderWidth;
