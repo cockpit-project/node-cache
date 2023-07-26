5985331f65dbfcc79e1390cfe43fd056f846e796
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-26 14:11:39 +0200

    8245eb3eed14f71c7db6b9bad4691b6d49eef5d3c991f019638457c28e32d07e  package.json

---
+++ @patternfly/react-tokens/dist/js/c_form_control_m_readonly_hover_BorderBottomColor.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_form_control_m_readonly_hover_BorderBottomColor = {
+  "name": "--pf-v5-c-form-control--m-readonly--hover--BorderBottomColor",
+  "value": "#8a8d90",
+  "var": "var(--pf-v5-c-form-control--m-readonly--hover--BorderBottomColor)"
+};
+exports["default"] = exports.c_form_control_m_readonly_hover_BorderBottomColor;
---
+++ @patternfly/react-tokens/dist/js/c_form_control_m_success_BorderBottomWidth.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_form_control_m_success_BorderBottomWidth = {
+  "name": "--pf-v5-c-form-control--m-success--BorderBottomWidth",
+  "value": "2px",
+  "var": "var(--pf-v5-c-form-control--m-success--BorderBottomWidth)"
+};
+exports["default"] = exports.c_form_control_m_success_BorderBottomWidth;
