4bc6152fbeebd41ba937fe8c37438f9f7bce11ae
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-26 08:12:40 +0200

    d062ddbdc403782b12c897809494cc0afb20667e7a2dd745153c9a36b5f3861b  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_hover_BorderBottomColor.js
@@ -0,0 +1,6 @@
+export const c_form_control_hover_BorderBottomColor = {
+  "name": "--pf-v5-c-form-control--hover--BorderBottomColor",
+  "value": "#06c",
+  "var": "var(--pf-v5-c-form-control--hover--BorderBottomColor)"
+};
+export default c_form_control_hover_BorderBottomColor;
