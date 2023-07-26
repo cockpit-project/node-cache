bf32b2729ac4ee58de7a58a24288ea5ab4136ed8
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-26 07:46:39 +0200

    fb6cff4f78c4a271565ea39cc52f7968ddb6da2b1cc9b2930dc8297bfa1ae56d  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_focus_PaddingBottom.js
@@ -0,0 +1,6 @@
+export const c_form_control_focus_PaddingBottom = {
+  "name": "--pf-v5-c-form-control--focus--PaddingBottom",
+  "value": "calc(0.375rem - 1px)",
+  "var": "var(--pf-v5-c-form-control--focus--PaddingBottom)"
+};
+export default c_form_control_focus_PaddingBottom;
