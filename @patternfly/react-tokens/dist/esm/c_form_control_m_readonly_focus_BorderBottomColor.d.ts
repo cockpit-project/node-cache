cb5e60a276c920abae15020f7e6976db1ea6121b
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-26 10:25:56 +0200

    a688077d995bfe99d60cab694660c9a2a25a02253f21ea6f5e4ac0d938adcd3a  package.json

---
+++ @patternfly/react-tokens/dist/esm/c_form_control_m_readonly_focus_BorderBottomColor.d.ts
@@ -0,0 +1,6 @@
+export const c_form_control_m_readonly_focus_BorderBottomColor: {
+  "name": "--pf-v5-c-form-control--m-readonly--focus--BorderBottomColor",
+  "value": "#8a8d90",
+  "var": "var(--pf-v5-c-form-control--m-readonly--focus--BorderBottomColor)"
+};
+export default c_form_control_m_readonly_focus_BorderBottomColor;
---
+++ @patternfly/react-tokens/dist/js/c_form_control_BorderWidth.d.ts
@@ -0,0 +1,6 @@
+export const c_form_control_BorderWidth: {
+  "name": "--pf-v5-c-form-control--BorderWidth",
+  "value": "1px",
+  "var": "var(--pf-v5-c-form-control--BorderWidth)"
+};
+export default c_form_control_BorderWidth;
