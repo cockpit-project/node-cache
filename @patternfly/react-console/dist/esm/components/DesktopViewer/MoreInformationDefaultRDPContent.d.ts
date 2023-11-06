79827f17f6f11ee4374a93ee5f3384cd3341178b
GitHub Workflow <cockpituous@cockpit-project.org>
2023-11-06 21:19:31 +0000

    fce0d4fb55c874894762f41a2a0fea2a1bd88740e32639746311dec724062916  package.json

---
+++ @patternfly/react-console/dist/esm/components/DesktopViewer/MoreInformationDefaultRDPContent.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface MoreInformationDefaultRDPContentProps {
+    textMoreRDPInfoContent?: string | React.ReactNode;
+}
+export declare const MoreInformationDefaultRDPContent: React.FunctionComponent<MoreInformationDefaultRDPContentProps>;
+//# sourceMappingURL=MoreInformationDefaultRDPContent.d.ts.map
