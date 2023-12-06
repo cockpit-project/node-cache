83ea3107145500dfd911557bffb640180a7dd51d
GitHub Workflow <cockpituous@cockpit-project.org>
2023-12-06 21:41:56 +0000

    651871809a9912c1561da9e26c57eb1ab348559dc10c82c77d6ffb99b8e2ad6e  package.json

---
+++ @patternfly/react-console/src/components/VncConsole/VncConsole.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { VncConsole } from './VncConsole';
+
+test('placeholder render test', () => {
+  const { asFragment } = render(<VncConsole host="my.unknown.host" />);
+  expect(asFragment()).toMatchSnapshot();
+});
