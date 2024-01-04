cb220a0dc1f1c6957a9076da6d95bef32f586b69
GitHub Workflow <cockpituous@cockpit-project.org>
2024-01-04 21:50:54 +0000

    8bbb878994d493c46585bc710a895789e4f61c89f731afd96bc9f75e4d9ba572  package.json

---
+++ @patternfly/react-console/dist/esm/components/common/constants.d.ts
@@ -0,0 +1,18 @@
+export declare const constants: {
+    NONE_TYPE: string;
+    SERIAL_CONSOLE_TYPE: string;
+    SPICE_CONSOLE_TYPE: string;
+    VNC_CONSOLE_TYPE: string;
+    RDP_CONSOLE_TYPE: string;
+    DESKTOP_VIEWER_CONSOLE_TYPE: string;
+    CONNECTING: string;
+    CONNECTED: string;
+    DISCONNECTED: string;
+    LOADING: string;
+    DEFAULT_VV_FILENAME: string;
+    DEFAULT_VV_MIMETYPE: string;
+    DEFAULT_RDP_FILENAME: string;
+    DEFAULT_RDP_MIMETYPE: string;
+    DEFAULT_RDP_PORT: number;
+};
+//# sourceMappingURL=constants.d.ts.map
