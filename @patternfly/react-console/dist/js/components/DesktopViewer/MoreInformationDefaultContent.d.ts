2a3542a263bf87191ed6e9ae27b00c36228a9132
GitHub Workflow <cockpituous@cockpit-project.org>
2024-01-30 04:28:29 +0000

    049fb37cd033955931932ee4509fe03285c1af359ef209ada5597cc7ca14f270  package.json

---
+++ @patternfly/react-console/dist/js/components/DesktopViewer/MoreInformationDefaultContent.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface MoreInformationDefaultContentProps {
+    textMoreInfoContent?: string | React.ReactNode;
+}
+export declare const MoreInformationDefaultContent: React.FunctionComponent<MoreInformationDefaultContentProps>;
+//# sourceMappingURL=MoreInformationDefaultContent.d.ts.map
